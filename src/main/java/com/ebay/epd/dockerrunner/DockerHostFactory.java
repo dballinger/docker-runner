1a942adf30ff25624e772033ff12bf97978dd510
Darren Ballinger <darren.ballinger@gmail.com>
2015-01-12 21:58:21 +0000

    make the tls host tests work

--- src/main/java/com/ebay/epd/dockerrunner/DockerHostFactory.java
+++ src/main/java/com/ebay/epd/dockerrunner/DockerHostFactory.java
@@ -1,8 +1,11 @@
 package com.ebay.epd.dockerrunner;
 
 import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerCertificateException;
+import com.spotify.docker.client.DockerCertificates;
 import com.spotify.docker.client.DockerClient;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
@@ -12,7 +15,11 @@ public class DockerHostFactory {
     public static DockerHost dockerHostForEnvironment(Map<String, String> env) {
         try {
             String hostKey = "DOCKER_HOST";
-            if (env.containsKey(hostKey)) {
+            String tlsVerifyKey = "DOCKER_TLS_VERIFY";
+            String certPathKey = "DOCKER_CERT_PATH";
+            if ("1".equals(env.get(tlsVerifyKey))) {
+                return new BootToDockerTlsHost(env.get(hostKey), env.get(certPathKey));
+            } else if (env.containsKey(hostKey)) {
                 return new BootToDockerHost(env.get(hostKey));
             } else {
                 return new NativeDockerHost();
@@ -64,9 +71,45 @@ public class DockerHostFactory {
         }
     }
 
+    public static class BootToDockerTlsHost implements DockerHost {
+
+        private final DefaultDockerClient dockerClient;
+        private final String host;
+
+        public BootToDockerTlsHost(String dockerHost, String certPath) throws URISyntaxException {
+            URI tcpUri = new URI(dockerHost);
+            host = tcpUri.getHost();
+            URI httpUri = new URI("https", null, host, tcpUri.getPort(), null, null, null);
+            try {
+                dockerClient = DefaultDockerClient.builder()
+                                .uri(httpUri)
+                                .dockerCertificates(new DockerCertificates(new File(certPath).toPath()))
+                                .build();
+            } catch (DockerCertificateException e) {
+                throw new DockerHostCertificateException(e);
+            }
+        }
+
+        @Override
+        public DockerClient client() {
+            return dockerClient;
+        }
+
+        @Override
+        public String host() {
+            return host;
+        }
+    }
+
     public static class DockerHostException extends RuntimeException {
         public DockerHostException(Throwable cause) {
             super("DOCKER_HOST env is an invalid url", cause);
+        }
+    }
+
+    public static class DockerHostCertificateException extends RuntimeException {
+        public DockerHostCertificateException(Throwable cause) {
+            super("Problem with the docker host cert path", cause);
         }
     }
 }
--- src/test/scala/com.ebay.epd.dockerrunner/DockerHostTest.scala
+++ src/test/scala/com.ebay.epd.dockerrunner/DockerHostTest.scala
@@ -2,7 +2,7 @@ package com.ebay.epd.dockerrunner
 
 import java.util
 
-import com.ebay.epd.dockerrunner.DockerHostFactory.{NativeDockerHost, DockerHostException, BootToDockerHost}
+import com.ebay.epd.dockerrunner.DockerHostFactory._
 import com.spotify.docker.client.messages.Info
 import org.scalatest.{Matchers, FlatSpec}
 
@@ -23,6 +23,21 @@ class DockerHostTest extends FlatSpec with Matchers {
     dockerHost.isInstanceOf[NativeDockerHost] should be(true)
     dockerHost.host() should be("localhost")
     dockerHost.client() == null should be(false)
+  }
+
+  it should "return TLS B2D host" in {
+    val env = new util.HashMap[String, String]()
+    env.put("DOCKER_HOST", "tcp://1.2.3.4:1234")
+    env.put("DOCKER_TLS_VERIFY", "1")
+    env.put("DOCKER_CERT_PATH", ".")
+    /*
+    Yeah, this is a bad test. DockerHostCertificateException only gets thrown from inside the TlsHost.
+    Unfortunately I can't find a suitable test without committing a real b2d pem.
+    Someone please improve this.
+     */
+    intercept[DockerHostCertificateException] {
+      val dockerHost = DockerHostFactory.dockerHostForEnvironment(env)
+    }
   }
 
   it should "throw if DOCKER_HOST is unvalid URI" in {
